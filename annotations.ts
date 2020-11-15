a2736a9e8ecb93d4c2afca237ce893d931cbe3b9
Sangsaka Wira <sangsakawira@Sangsakas-MacBook-Pro.local>
2020-11-15 09:45:24 +0700

    more annotations

---
+++ annotations.ts
@@ -0,0 +1,16 @@
+// Anotasi untuk menentukan tipe data dari variabel
+// meminimalisir error
+
+const apples: number = 5;
+const speed: string = "Yes";
+const hasPrev: boolean = true;
+
+const isNull: null = null;
+
+const now:Date = new Date();
+
+const myArray:string[] = ["red","blue"]
+
+console.log(apples)
+console.log(speed)
+console.log(hasPrev)
