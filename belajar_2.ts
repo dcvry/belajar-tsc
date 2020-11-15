de755e49ddb3a5478143c93665fcf3bac3b08448
Sangsaka Wira <sangsakawira@Sangsakas-MacBook-Pro.local>
2020-11-15 09:30:42 +0700

    pertemuan kedua

---
+++ belajar_2.ts
@@ -0,0 +1,13 @@
+const today = new Date()
+
+console.log(today.getDate())
+
+class Color {
+    getColor(){
+        return "Red"
+    }
+}
+
+const rumah = new Color()
+
+console.log(rumah.getColor())
